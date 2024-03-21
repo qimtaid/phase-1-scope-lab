7517bbfb3e2bce84e9e5fa51cd5d1b1515fbf9f5
Dan Koskei <qimtaid@gmail.com>
2024-03-21 16:57:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+var customerName = 'bob'; // Declaring customerName in global scope using the var keyword
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase(); // Accessing and modifying the global customerName variable
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Declaring bestCustomer in global scope
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'; // Accessing and modifying the global bestCustomer variable
+}
+
+const leastFavoriteCustomer = 'someone'; // Declaring leastFavoriteCustomer as a constant in global scope
+
+function changeLeastFavoriteCustomer() {
+    // Attempting to change the constant leastFavoriteCustomer
+    // This will result in an error being thrown as you cannot reassign a constant
+    leastFavoriteCustomer = 'someone else';
+}
+
+module.exports = { 
+    customerName, 
+    upperCaseCustomerName, 
+    setBestCustomer, 
+    overwriteBestCustomer, 
+    leastFavoriteCustomer, 
+    changeLeastFavoriteCustomer 
+};
+
